0f9c54827fd0f7114f15d93169e0fcb53512126f
CARLOS ANDRADE <charly.2701997@gmail.com>
2020-11-26 18:47:51 -0500

    commit1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HeroesApp } from './HeroesApp';
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
+ReactDOM.render(
+    <HeroesApp />,
+  document.getElementById('root')
+);
+
